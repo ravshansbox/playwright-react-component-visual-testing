a6c111927458ebceb7408ebc5302310fbc052a19
Ravshan Samandarov <ravshansbox@gmail.com>
2023-05-08 16:23:45 +0300

    initial commit

---
+++ playwright-ct.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig, devices } from '@playwright/experimental-ct-react';
+
+export default defineConfig({
+  testDir: './src',
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+});
